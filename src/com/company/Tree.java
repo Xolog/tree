5c00124bca76fea62a2eef0435d263233f5c563c
Xolog <stalk200213@gmail.com>
2021-09-10 01:08:22 +0300

    Update Tree.java
    Написан метод вставки узла и вывода всех элементов, исправлен метод поиска элемента

--- src/com/company/Tree.java
+++ src/com/company/Tree.java
@@ -8,12 +8,14 @@ public class Tree {
      */
     public Node find(int key) {
         Node current = root;
+
         while (current.key != key) {
             if (current.key < key) {
-                current = current.rightChild;
-            } else {
                 current = current.leftChild;
+            } else {
+                current = current.rightChild;
             }
+
             if (current == null) {
                 return null;
             }
@@ -21,5 +23,50 @@ public class Tree {
         return current;
     }
 
+    /*
+      Вставка в дерево. Суть таже что и поиск
+      Только вместо вывода элемента к нему левым или правым потомком
+      добавляем новый элемент
+     */
+    public void insert(int key, String data) {
+        Node node = new Node();
+        node.key = key;
+        node.data = data;
 
+        if (root == null) {
+            root = node;
+        } else {
+            Node current = root;
+            Node parent;
+            while (true) {
+                parent = current;
+                if (key < parent.key) {
+                    current = current.leftChild;
+
+                    if (current == null) {
+                        parent.leftChild = node;
+                        return;
+                    }
+                } else {
+                    current = current.rightChild;
+
+                    if (current == null) {
+                        parent.rightChild = node;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    /*
+      Вывод всех элементов дерева методом асиметричного обхода
+     */
+    public void print(Node startNode) {
+        if (startNode != null) {        //условие сработает, когда мы достигним конца дерева и потомков не останется
+            print(startNode.leftChild);     //рекурсивно вызываем левых потомков
+            startNode.printNode();      //вызов метода принт
+            print(startNode.rightChild);    //вызов правых потомков
+        }
+    }
 }
